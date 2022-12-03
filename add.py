4cbde2c9ef3c3ef5b7b0e31b530b2280ae5c16d3
Vivek <60600384+vivekkumar5059@users.noreply.github.com>
2022-12-03 12:15:39 +0530

    Update add.py

--- add.py
+++ add.py
@@ -4,9 +4,13 @@ num1=int(input())
 print("enter number n2")
 num2=int(input())
 print("enter  number n4")
+num3=int(input())
+
+print|("enter number n5")
+
 #commented by rohit
-num=int(input())
-num3= num1+num2+num4
+num4=int(input())
+num3= num1+num2+num4+num5
 print("result")
 print(num3)
 
